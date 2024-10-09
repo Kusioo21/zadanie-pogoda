2379b197271de98a2e6f9d16ec3172f761c54807
3981864 <3981864@TE-GDA-D-D21E17>
2024-10-09 10:12:40 +0200

    Dodaj pliki projektów.

---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,74 @@
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace Pogoda
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer;
+        private readonly HttpClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new HttpClient { BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast?latitude=54.36&longitude=18.64&current=temperature_2m,relative_humidity_2m,precipitation") };
+            InitializeTimer();
+            WeatherData();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer
+            {
+                Interval = 900000 // 15 minut
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            WeatherData();
+        }
+
+        private async void WeatherData()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                JObject weatherData = JObject.Parse(result);
+
+                UpdateWeatherDisplay(weatherData);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wyst¹pi³ b³¹d podczas pobierania danych: {ex.Message}");
+            }
+        }
+
+        private void UpdateWeatherDisplay(JObject weatherData)
+        {
+            var currentWeather = weatherData["current"];
+            var temperature = currentWeather["temperature_2m"];
+            var humidity = currentWeather["relative_humidity_2m"];
+            var precipitation = currentWeather["precipitation"];
+
+            Temperature.Text = $"Temperatura: {temperature} °C";
+            Humidity.Text = $"Wilgotnoœæ: {humidity} %";
+            Precipitation.Text = $"Opady: {precipitation} mm";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Precipitation_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
